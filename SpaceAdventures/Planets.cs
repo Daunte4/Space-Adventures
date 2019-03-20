8f8613cde70fcdffa02bbc6ca6a7abc1b4280270
Aquileo Reyes <aquileoreyes@gmail.com>
2019-03-20 09:09:49 -0400

    deleted three unnecessary classes

--- SpaceAdventures/Planets.cs
+++ SpaceAdventures/Planets.cs
@@ -15,6 +15,49 @@ namespace SpaceAdventures
             PlanetInfomaton();
             //TravelToAPlanet();
         }
+        public void StartLocation()
+        {
+            Console.WriteLine("\nYou are starting at the planet earth.\n");
+            Console.Read();
+        }
+        public void ShipName()
+        {
+            Console.WriteLine("\nYour ship is called: The Lady Grey");
+            Console.Read();
+        }
+        public void ShipStatus()
+        {
+            Console.WriteLine("\nYour ship that you started with can only travel 500 light years.\n");
+            Console.Read();
+        }
+
+
+        public void ShipLocation()
+        {
+            string name = "";
+            //Console.Clear();
+            bool check = false;
+            while (check == false)
+
+            {
+                Console.WriteLine("\nSelect from the options below.\n" + "1. Disembarking " + " 2. Plotting a course");
+                name = Console.ReadLine();
+                switch (name)
+
+                {
+                    case "1":
+                        Console.WriteLine("Disembarking...");
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        Console.WriteLine("Plotting a course...");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+
+
+        }
 
 
         //This a switch statment that talks about Planet information
